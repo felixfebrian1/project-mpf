a110481d16b454f036e8b2361bfe9f6608df013e
felixfebrian1 <48092164+felixfebrian1@users.noreply.github.com>
2019-06-22 06:06:44 +0700

    Create pencilline.cs

---
+++ pencilline.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pencilline : MonoBehaviour {
+
+    //to refer to our prefab pencilline
+    public GameObject obj;
+
+    float lastx = 0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if we have moved far enough make a new pencilline
+
+        if (transform.position.x > (lastx + 0.02f))
+        {
+
+
+            Instantiate(obj, transform.position, Quaternion.identity);
+            lastx = transform.position.x;
+
+            lastx = transform.position.x;
+
+        }
+
+    }
+}
